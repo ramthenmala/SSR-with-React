ca8c34be603e969139ab524f983ce012bd4cff5e
Ram kumar <webbieram@outlook.com>
2022-07-09 21:56:10 +0530

    client bootup with hydrate method

--- src/client/client.js
+++ src/client/client.js
@@ -1,3 +1,7 @@
 // Startup point for the client side application
 
-console.log('Hello from Client');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Home from './components/Home'
+
+ReactDOM.hydrate(<Home />, document.getElementById('root'));
--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Home = () => {
     return (
-      <>
+      <div>
         <h1>Implemented es6</h1>
         <button onClick={() => console.log('Helo')}>Click me</button>
-      </>
+      </div>
     );
 }
 
