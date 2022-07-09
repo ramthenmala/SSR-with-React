fc2d0b68a604ae930d5b8649ae25cd3500405035
Ram kumar <webbieram@outlook.com>
2022-07-09 23:22:01 +0530

    added webpack node externals

--- webpack.server.js
+++ webpack.server.js
@@ -1,7 +1,7 @@
 const path = require('path')
 const merge = require('webpack-merge');
 const baseConfig = require('./webpack.base');
-
+const webpackNodeExternals = require('webpack-node-externals');
 const config = {
   // Inform webpack we are building a bundle for
   // nodejs, not for browser
@@ -11,6 +11,7 @@ const config = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
+  externals: [webpackNodeExternals()],
 };
 
 module.exports = merge(baseConfig, config);
